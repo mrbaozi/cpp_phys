2fe1e85611da7b89d5a556074e573028fe3ed4c2
Yannick Augenstein <yannick.augenstein@me.com>
2018-04-20 23:35:40 +0200

    Add sheet01

---
+++ sheet01/task03.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, const char **argv)
+{
+    cout << "Exponential\n" << endl;
+
+    float x;
+    if (argc < 2)
+    {
+        cout << "Input: ";
+        cin >> x;
+    }
+    else
+    {
+        x = atof(argv[1]);
+    }
+
+    cout << "exp(" << x << ") = " << exp(x) << endl;
+
+    return 0;
+} //main
