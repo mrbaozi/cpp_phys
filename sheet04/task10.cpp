ef7887f8755aba6a4d945ea3be4d6ce543c40e9f
Yannick Augenstein <yannick.augenstein@me.com>
2018-05-11 11:56:39 +0200

    Finish task10 (including bonus)

--- sheet04/task10.cpp
+++ sheet04/task10.cpp
@@ -1,22 +1,57 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <cassert>
 
 using namespace std;
+
+int collatz(int a, vector<int>& results)
+{
+    results.push_back(a);
+    if (a == 1)
+    {
+        return results.size();
+    }
+    if (a % 2 == 0)
+    {
+        return collatz(a / 2, results);
+    }
+    else
+    {
+        return collatz(3 * a + 1, results);
+    }
+}
 
 int main(int argc, const char **argv)
 {
     cout << "Aufgabe 10: 3n+1 - Problem\n" << endl;
 
-    double x;
+    int a {0};
+    vector<int> collatz_results;
     if (argc < 2)
     {
-        cout << "Input: ";
-        cin >> x;
+        vector<int> lengths;
+        for (int a = 2; a < 100; ++a)
+        {
+            lengths.push_back(collatz(a, collatz_results));
+            collatz_results.clear();
+        }
+        a = distance(lengths.begin(), max_element(lengths.begin(), lengths.end())) + 2;
+        cout << "Longest sequence for a = " << a << endl;
     }
     else
     {
-        x = atof(argv[1]);
+        a = atoi(argv[1]);
+        assert(a > 1);
+        cout << "Start value: " << a << endl;
     }
+
+    collatz(a, collatz_results);
+
+    cout << "Length of sequence: " << collatz_results.size() << endl;
+    cout << "Maximum value: ";
+    cout << *max_element(collatz_results.begin() + 1, collatz_results.end()) << endl;
 
     return 0;
 } //main
