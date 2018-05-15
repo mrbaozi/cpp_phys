77119a8f36c703c56b074bd1b76cc3d548ead3c0
Yannick Augenstein <yannick.augenstein@me.com>
2018-05-15 11:56:56 +0200

    Finish task11

--- sheet04/task11.cpp
+++ sheet04/task11.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <string>
-#include <vector>
 #include <cassert>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -17,17 +17,58 @@ int main(int argc, const char **argv)
     ifstream file {file_name};
     assert(file.is_open());
 
-    vector<double> mat;
-    double num {0};
-    while (file >> num)
+    // Input
+    double mat[3][3];
+    for (int ii = 0; ii < 3; ++ii)
     {
-        mat.emplace_back(num);
+        for (int jj = 0; jj < 3; ++jj)
+        {
+            file >> mat[ii][jj];
+        }
+    }
+    file.close();
+
+    // Determinant (Sarrus)
+    double det {0};
+    for (int ii = 0; ii < 3; ++ii)
+    {
+        double pr1 {1};
+        double pr2 {1};
+        for (int jj = 0; jj < 3; ++jj)
+        {
+            pr1 *= mat[jj][(ii + jj) % 3];
+            pr2 *= mat[jj][(ii + 3 - jj) % 3];
+        }
+        det += pr1 - pr2;
     }
 
-    for (auto const& val: mat)
+    // Norm
+    double norm {0};
+    for (int jj = 0; jj < 3; ++jj)
     {
-        cout << val << endl;
+        double sum {0};
+        for (int ii = 0; ii < 3; ++ii)
+        {
+            sum += abs(mat[ii][jj]);
+        }
+        if (sum >= norm)
+        {
+            norm = sum;
+        }
     }
+
+    // Output
+    cout << "Matrix: " << endl;
+    for (int ii = 0; ii < 3; ++ii)
+    {
+        for (int jj = 0; jj < 3; ++jj)
+        {
+            cout << setw(6) << mat[ii][jj];
+        }
+        cout << endl;
+    }
+    cout << "Determinante: " << det << endl;
+    cout << "Spaltensummennorm: " << norm << endl;
 
     return 0;
 } //main
